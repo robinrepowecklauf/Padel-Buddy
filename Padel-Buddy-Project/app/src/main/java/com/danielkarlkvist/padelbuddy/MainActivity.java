e3fff36e3975e1f1bf2b51f7efa20c12f09572c4
Daniel Karlkvist <danielkarlkvist@gmail.com>
2019-09-07 11:23:08 +0200

    Added Android Studio project

---
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.danielkarlkvist.padelbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
