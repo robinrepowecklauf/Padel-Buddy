152e8e66e535a546eff627688fe710c866a5da2d
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2019-09-18 16:29:09 +0200

    created constructor for FinishedGame

--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/FinishedGame.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/FinishedGame.java
@@ -1,5 +1,13 @@
 package com.danielkarlkvist.padelbuddy.Model;
 
+import android.location.Location;
+
+import java.util.Date;
+
 public class FinishedGame extends Game {
 
+
+    public FinishedGame(Player[] players, Location location, Date date) {
+        super(players, location, date);
+    }
 }
