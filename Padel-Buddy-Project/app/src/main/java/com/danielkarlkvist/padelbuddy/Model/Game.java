e1849a75556c449295b8b3b38f31080800ace30c
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2019-09-18 16:12:56 +0200

    finished Game so far from design model

--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/Game.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/Game.java
@@ -1,4 +1,21 @@
 package com.danielkarlkvist.padelbuddy.Model;
 
+import android.location.Location;
+
+import java.util.Date;
+
 public abstract class Game {
+    private Player[] players;
+    private Location location;
+    private Date date;
+
+    // TODO decide return type
+    private void getAvgSkillLevel() {
+
+    }
+
+    // TODO decide return type
+    private void getAvgProfileRating() {
+
+    }
 }
