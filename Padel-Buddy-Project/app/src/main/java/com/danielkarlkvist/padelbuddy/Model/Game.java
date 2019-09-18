8332341e2ceca41ed7802680ae409450a9a6e5f3
Carl-Johan Björnson <calle.bjornson@gmail.com>
2019-09-18 16:59:57 +0200

    - Added Setters and Getters to Game. - Added createAd method to PadelBuddy.

--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/Game.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/Game.java
@@ -23,4 +23,30 @@ abstract class Game {
     private void getAvgProfileRating() {
 
     }
+
+    //Setters and Getters.
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddy.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddy.java
@@ -1,6 +1,9 @@
 package com.danielkarlkvist.padelbuddy.Model;
 
+import android.location.Location;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class PadelBuddy {
@@ -8,4 +11,15 @@ public class PadelBuddy {
     private List<GameAd> gameAds = new ArrayList<>();
     private List<FinishedGame> finishedGames = new ArrayList<>();
     private Player player;
+
+    public PadelBuddy(Player player) {
+        this.player = player;
+    }
+
+    //TODO Command query????
+    GameAd createAd(Location location, Date date) {
+        GameAd gameAd = new GameAd(location, date);
+        gameAd.getPlayers()[0] = player;
+        return gameAd;
+    }
 }
