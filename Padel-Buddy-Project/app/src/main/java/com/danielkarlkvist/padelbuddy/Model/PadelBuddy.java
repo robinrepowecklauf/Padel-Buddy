243a143c1936518e5ec4e6d2539b3ca26c00c6a9
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2019-09-25 09:47:49 +0200

    refactored according to current design model - deleted FinishedGame and GameAd classes

--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddy.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddy.java
@@ -8,25 +8,24 @@ import java.util.List;
 
 public class PadelBuddy {
 
-    private List<GameAd> gameAds = new ArrayList<>();
-    private List<FinishedGame> finishedGames = new ArrayList<>();
+    private List<Game> games = new ArrayList<>();
     private Player player;
 
     public PadelBuddy(Player player) {
         this.player = player;
     }
 
-    //TODO Command query?
+    // TODO Command query?
     void createAd(Location location, Date date) {
-        GameAd gameAd = new GameAd(location, date);
-        gameAd.getPlayers()[0] = player;
-        gameAds.add(gameAd);
+        Game game = new Game(location, date);
+        game.getPlayers()[0] = player;
+        games.add(game);
     }
 
-    void deleteAd(GameAd gameAd) {
-        if (finishedGames.contains(gameAd)) {
-            finishedGames.remove(gameAd);
+    void deleteAd(Game game) {
+        if (games.contains(game)) {
+            games.remove(game);
         }
-        // TODO Error message?
+        // TODO Error message? FancyToast Library?? Finns i slack
     }
 }
