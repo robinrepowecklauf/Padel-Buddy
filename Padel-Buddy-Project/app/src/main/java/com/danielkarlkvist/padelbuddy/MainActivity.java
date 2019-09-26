4c9bc00d9d9f083a66b74a76f5098f889afb83b9
Daniel Karlkvist <danielkarlkvist@gmail.com>
2019-09-26 09:01:57 +0200

    added game_xml

--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/MainActivity.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     private GamesFragmentController gamesFragmentController;
     private ProfileFragmentController profileFragmentController;
 
-
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationViewListener =
             // region bottomNavigationViewListener
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -74,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     private void initializeBottomNavigationView() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationViewListener);
-        // finns för att home-fliken ska få rätt fragment-container
+        // finns för att home-fliken ska få rätt fragment-container vid start
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
     }
 }
