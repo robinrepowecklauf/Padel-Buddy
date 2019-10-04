a1ade4fc68039ecb195474312fb7c68c37fe3e2c
Fredrik <fredriklilliecreutz@gmail.com>
2019-10-04 15:01:19 +0200

    Created UpcomingGameFragment, HistoryGameFragment and two XML-files for these classes

---
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Controller/UpcomingGameFragment.java
@@ -0,0 +1,25 @@
+package com.danielkarlkvist.padelbuddy.Controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.danielkarlkvist.padelbuddy.Model.PadelBuddy;
+import com.danielkarlkvist.padelbuddy.R;
+
+public class UpcomingGame extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_upcoming_game, container, false);
+
+
+        return rootView;
+    }
+}
+
