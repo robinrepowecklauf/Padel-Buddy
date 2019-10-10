795538b0a378c4679d5928462d9211cd11e0a38f
Fredrik <fredriklilliecreutz@gmail.com>
2019-10-10 17:42:08 +0200

    updated test createAdGameExist

--- Padel-Buddy-Project/app/src/test/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddyTest.java
+++ Padel-Buddy-Project/app/src/test/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddyTest.java
@@ -2,6 +2,8 @@ package com.danielkarlkvist.padelbuddy.Model;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.Assert.*;
@@ -10,11 +12,13 @@ public class PadelBuddyTest {
 
     private PadelBuddy padelBuddy;
 
+
     @Test
     public void createAd_GameExist_ReturnsTrue() {
         padelBuddy = PadelBuddy.getInstance();
+        int temp = padelBuddy.getGames().size();
         padelBuddy.createAd("Göteborg", new Date(2019, 11, 12));
-        assertTrue(padelBuddy.getGames().size() > 0);
+        assertTrue(padelBuddy.getGames().size() == temp+1);
     }
 
 }
