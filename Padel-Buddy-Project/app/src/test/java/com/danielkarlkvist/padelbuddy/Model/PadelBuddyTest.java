213989fac0307aaa3415ffa980cafa3cf338b470
Fredrik <fredriklilliecreutz@gmail.com>
2019-10-10 17:09:57 +0200

    added new PadelBuddyTest and Model package

---
+++ Padel-Buddy-Project/app/src/test/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddyTest.java
@@ -0,0 +1,15 @@
+package com.danielkarlkvist.padelbuddy.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PadelBuddyTest {
+
+    private PadelBuddy padelBuddy;
+
+    @Test
+    public void getUpcomingGames() {
+
+    }
+}
