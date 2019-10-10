684e888e7cafd97044ca74ad4b71e233faefd6cc
Fredrik <fredriklilliecreutz@gmail.com>
2019-10-10 18:04:44 +0200

    created test createAd_PlayerExist

--- Padel-Buddy-Project/app/src/test/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddyTest.java
+++ Padel-Buddy-Project/app/src/test/java/com/danielkarlkvist/padelbuddy/Model/PadelBuddyTest.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.crypto.BadPaddingException;
+
 import static org.junit.Assert.*;
 
 public class PadelBuddyTest {
@@ -19,6 +21,12 @@ public class PadelBuddyTest {
         int temp = padelBuddy.getGames().size();
         padelBuddy.createAd("Göteborg", new Date(2019, 11, 12));
         assertTrue(padelBuddy.getGames().size() == temp+1);
+    }
+    @Test
+    public void createAd_PlayerExist_ReturnsTrue() {
+        padelBuddy = PadelBuddy.getInstance();
+        padelBuddy.createAd("Göteborg", new Date(2019, 11, 12));
+        assertTrue(padelBuddy.getGames().get(padelBuddy.getGames().size()-1).hasPlayer());
     }
 
 }
