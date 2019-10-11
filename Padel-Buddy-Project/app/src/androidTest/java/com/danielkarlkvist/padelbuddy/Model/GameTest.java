0e088b16f530ecdd52b34ef97bca12a922b09307
Fredrik <fredriklilliecreutz@gmail.com>
2019-10-11 17:51:32 +0200

    Created testmethod addPlayer_onePlayer_ReturnsTrue

--- Padel-Buddy-Project/app/src/androidTest/java/com/danielkarlkvist/padelbuddy/Model/GameTest.java
+++ Padel-Buddy-Project/app/src/androidTest/java/com/danielkarlkvist/padelbuddy/Model/GameTest.java
@@ -2,12 +2,26 @@ package com.danielkarlkvist.padelbuddy.Model;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
 
+
     @Test
-    public void addPlayer() {
+    public void addPlayer_onePlayerIsAdded_ReturnsTrue() {
+        Player player1 = new Player("Fredrik", "Axelsson", "test@gmail.com", "123", "lorem ", 14, 5);
+        Player player2 = new Player("Axel", "Axelsson", "test@gmail.com", "123", "lorem ", 14, 5);
+        Game game = new Game(player1, "Gltk", new Date(2019, 11, 05));
+        game.addPlayer(player2);
+        Player[] players = game.getPlayers();
+        assertTrue(players[0].getFirstname()=="Fredrik");
+        assertTrue(players[1].getFirstname()=="Axel");
+        assertTrue(players[2]==null);
+        assertTrue(players[3]==null);
     }
-    
+
 }
