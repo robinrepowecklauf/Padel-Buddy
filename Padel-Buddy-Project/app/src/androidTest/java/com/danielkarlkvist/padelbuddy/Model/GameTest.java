637dd559dfc3ecd868eda001c9997ce6edc61669
Fredrik <fredriklilliecreutz@gmail.com>
2019-10-11 14:51:32 +0200

    Removed method hasPlayer in Game and edited the testmethods for Padelbuddy

---
+++ Padel-Buddy-Project/app/src/androidTest/java/com/danielkarlkvist/padelbuddy/Model/GameTest.java
@@ -0,0 +1,13 @@
+package com.danielkarlkvist.padelbuddy.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void addPlayer() {
+    }
+    
+}
