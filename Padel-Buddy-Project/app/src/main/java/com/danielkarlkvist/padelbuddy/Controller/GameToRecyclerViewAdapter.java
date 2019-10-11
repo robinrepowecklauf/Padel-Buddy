a1fef6b7786787caa3fde85d642597c4b7f1c0e6
Daniel Karlkvist <danielkarlkvist@gmail.com>
2019-10-11 12:13:48 +0200

    Turned the 4 variables of playerImagesViews into an array instead and moved the logic into a for-loop
    Co-authored-by: Fredrik Lilliecreutz <fredriklilliecreutz@gmail.com>

--- Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Controller/GameToRecyclerViewAdapter.java
+++ Padel-Buddy-Project/app/src/main/java/com/danielkarlkvist/padelbuddy/Controller/GameToRecyclerViewAdapter.java
@@ -38,12 +38,8 @@ public class GameToRecyclerViewAdapter extends RecyclerView.Adapter<GameToRecycl
         TextView dateTextView;
         TextView skillLevelTextView;
 
-        ImageView player1ImageView;
-        ImageView player2ImageView;
-        ImageView player3ImageView;
-        ImageView player4ImageView;
-
         TextView[] playerNameTextViews = new TextView[4];
+        ImageView[] playerImagesViews = new ImageView[4];
         RatingBar[] playerRatingBars = new RatingBar[4];
 
         GameAdViewHolder(@NonNull View itemView) {
@@ -54,10 +50,10 @@ public class GameToRecyclerViewAdapter extends RecyclerView.Adapter<GameToRecycl
 
             skillLevelTextView = itemView.findViewById(R.id.skilllevel_textview);
 
-            player1ImageView = itemView.findViewById(R.id.player1_imageview);
-            player2ImageView = itemView.findViewById(R.id.player2_imageview);
-            player3ImageView = itemView.findViewById(R.id.player3_imageview);
-            player4ImageView = itemView.findViewById(R.id.player4_imageview);
+            playerImagesViews[0] = itemView.findViewById(R.id.player1_imageview);
+            playerImagesViews[1] = itemView.findViewById(R.id.player2_imageview);
+            playerImagesViews[2] = itemView.findViewById(R.id.player3_imageview);
+            playerImagesViews[3] = itemView.findViewById(R.id.player4_imageview);
 
             playerNameTextViews[0] = itemView.findViewById(R.id.player1_name_textview);
             playerNameTextViews[1] = itemView.findViewById(R.id.player2_name_textview);
@@ -99,20 +95,16 @@ public class GameToRecyclerViewAdapter extends RecyclerView.Adapter<GameToRecycl
         // Set skill level
         holder.skillLevelTextView.setText("Nybörjare");
 
-        // Set images of players
-        holder.player1ImageView.setImageResource(R.drawable.profile_picture);
-        holder.player2ImageView.setImageResource(R.drawable.profile_picture);
-        holder.player3ImageView.setImageResource(R.drawable.profile_picture);
-        holder.player4ImageView.setImageResource(R.drawable.waitning_for_player_picture);
-
         // Set name and rating for all (4) players
         for (int i = 0; i < currentGame.getPlayers().length; i++) {
             Player player = currentGame.getPlayers()[i];
             if (player != null) {
                 holder.playerNameTextViews[i].setText(player.getFirstname());
+                holder.playerImagesViews[i].setImageResource(R.drawable.profile_picture);
                 holder.playerRatingBars[i].setRating(player.getProfileRating());
             } else {
                 holder.playerNameTextViews[i].setText("Tillgänglig");
+                holder.playerImagesViews[i].setImageResource(R.drawable.waitning_for_player_picture);
             }
         }
     }
